61b08d9fd14a3e2aa5561a3c00e578b054c44e24
Robin D <mr.robin.dunn@gmail.com>
2016-08-11 16:30:02 +0800

    Added checks to prevent interruption to menu animation.

--- burgermenu.js
+++ burgermenu.js
@@ -1,17 +1,12 @@
-/**
- * hamburger.js
- *
- * Mobile Menu Hamburger
- * =====================
- * A hamburger menu for mobile websites
- *
- * Created by Thomas Zinnbauer YMC AG  |  http://www.ymc.ch
- * Date: 21.05.13
- */
+var menuOpening = 0;
+var menuClosing = 0;
 
 function openMenu() {
-    console.log('hello');
-        
+    
+    if(menuOpening == 1 || menuClosing == 1) return;
+    
+    menuOpening = 1;
+    
     $('#pageContent').css('min-height', $(window).height());
 
     //$('nav').css('opacity', 1);
@@ -32,15 +27,19 @@ function openMenu() {
 
     //set margin for the whole container with a jquery UI animation
     $("#pageContent").animate({"marginLeft": ["-200px", 'easeOutExpo']}, {
-        duration: 700,
+        duration: 600,
         complete: function () {
             $('nav').css('opacity', 1);
+            menuOpening = 0;
         }
     });
 } 
  
 function closeMenu(){
 
+    if(menuOpening == 1 || menuClosing == 1) return;
+
+    menuClosing = 1;
     $('nav').css('opacity', 0);
 
     //enable all scrolling on mobile devices when menu is closed
@@ -48,11 +47,13 @@ function closeMenu(){
 
     //set margin for the whole container back to original state with a jquery UI animation
     $("#pageContent").animate({"marginLeft": ["0", 'easeOutExpo']}, {
-        duration: 700,
+        duration: 600,
         complete: function () {
             $('#pageContent').css('width', 'auto');
             $('#contentLayer').css('display', 'none');
             $('#pageContent').css('min-height', 'auto');
+            menuClosing = 0;
+            $('nav').css('opacity', 0);
         }
     });
  }
